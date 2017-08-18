7d64268bdb688a4bbb5c246822749070ed4ad8ec
meena1504 <30655491+meena1504@users.noreply.github.com>
2017-08-18 20:31:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/article-one', function(req, res){
+app.get('/article-one', function(req, res){
     res.send('article one is requested and served');
     
 });
-    app.get('/ui/article-two', function(req, res){
+    app.get('/article-two', function(req, res){
     res.send('article two is requested and served');
     
     });
     
-    app.get('/ui/article-three', function(req, res){
+    app.get('/article-three', function(req, res){
     res.send('article three is requested and served');
 });
 app.get('/ui/style.css', function (req, res) {
