10c80fdb5a4e026f0c3fbe44e5679232eaaf70e6
meena1504 <30655491+meena1504@users.noreply.github.com>
2017-08-19 17:52:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,19 +84,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function(req, res){
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-    
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-    app.get('/article-two', function(req, res){
-    res.send('article two is requested and served');
-    
-    });
-    
-    app.get('/article-three', function(req, res){
-    res.send('article three is requested and served');
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
