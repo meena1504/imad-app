bdaa6fbffb9d0cafb17638b3124aabf81161ffb2
meena1504 <30655491+meena1504@users.noreply.github.com>
2017-08-22 15:12:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'new value';
+//move the image
+
+var img = document.getElementById('madi');
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+    
+};
