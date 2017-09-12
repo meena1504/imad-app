21867ca7ccc533a7b6f6bee6c98bdaeff9688a95
meena1504 <30655491+meena1504@users.noreply.github.com>
2017-09-12 17:12:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
 
+var config = {
+    user: 'meenascorpio1504',
+    database: 'meenascorpio1504',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: password.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -82,6 +90,20 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req , res){
+   //make a select request
+   //return a response with the results
+   pool.query('SELECT * FROM test', function(err, result){
+      if(err){
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.sringify(result));
+      }
+       
+   });
 });
 
 var counter = 0;
