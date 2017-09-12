15764815a76bd1a3387185c80cd62beff7651248
meena1504 <30655491+meena1504@users.noreply.github.com>
2017-09-12 17:23:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/test-db', function(req , res){
       if(err){
           res.status(500).send(err.toString());
       } else {
-          res.send(JSON.sringify(result));
+          res.send(JSON.sringify(result.rows));
       }
        
    });
